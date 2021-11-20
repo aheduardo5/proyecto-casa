362a7dd64799f602cba117d0774e005598284e81
Eduardo Aguilar <aheduardo5@gmail.com>
2021-11-20 11:07:13 -0600

    Dark mode: finalizado. Se finaliza el diseño dark mode

--- src/js/app.js
+++ src/js/app.js
@@ -9,7 +9,7 @@ function darkMode() {
     const botonDarkMode = document.querySelector('.dark-mode-boton');
 
     botonDarkMode.addEventListener('click', function() {
-        document.body.classList.toggle('dark-mode')
+        document.body.classList.toggle('dark-mode');
     });
 }
 
